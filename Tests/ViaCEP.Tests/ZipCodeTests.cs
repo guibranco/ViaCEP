4d9b439fec67f8da53362740009cba4c9084563e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-03-28 02:15:43 -0300

    Converage 100%

--- Tests/ViaCEP.Tests/ZipCodeTests.cs
+++ Tests/ViaCEP.Tests/ZipCodeTests.cs
@@ -1,3 +1,6 @@
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace ViaCEP.Tests
 {
     using Xunit;
@@ -16,10 +19,34 @@ namespace ViaCEP.Tests
             var result = ViaCEPClient.Search("03177010");
             Assert.NotNull(result);
             Assert.Equal("Rua Doutor João Batista de Lacerda", result.Street);
+            Assert.Equal(string.Empty, result.Complement);
             Assert.Equal("Quarta Parada", result.Neighborhood);
             Assert.Equal("São Paulo", result.City);
             Assert.Equal("SP", result.StateInitials);
+            Assert.Equal(3550308, result.IBGECode);
+            Assert.Equal(1004, result.GIACode);
+            Assert.Equal(string.Empty, result.Unit);
         }
+
+        /// <summary>
+        /// Validates the search by zip code asynchronous.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task ValidateSearchByZipCodeAsync()
+        {
+            var result = await ViaCEPClient.SearchAsync("03165970", CancellationToken.None);
+            Assert.NotNull(result);
+            Assert.Equal("Rua da Mooca 3107", result.Street);
+            Assert.Equal(string.Empty, result.Complement);
+            Assert.Equal("Mooca", result.Neighborhood);
+            Assert.Equal("São Paulo", result.City);
+            Assert.Equal("SP", result.StateInitials);
+            Assert.Equal(3550308, result.IBGECode);
+            Assert.Equal(1004, result.GIACode);
+            Assert.Equal(string.Empty, result.Unit);
+        }
+
 
         /// <summary>
         /// Validates if the search by zip code don't throw a exception if the address doesn't have a gia code 
@@ -30,7 +57,14 @@ namespace ViaCEP.Tests
         {
             var result = ViaCEPClient.Search("22795641");
             Assert.NotNull(result);
+            Assert.Equal("Avenida Henfil", result.Street);
+            Assert.Equal("até 600/601", result.Complement);
+            Assert.Equal("Recreio dos Bandeirantes", result.Neighborhood);
+            Assert.Equal("Rio de Janeiro", result.City);
+            Assert.Equal("RJ", result.StateInitials);
+            Assert.Equal(3304557, result.IBGECode);
             Assert.Null(result.GIACode);
+            Assert.Equal(string.Empty, result.Unit);
         }
     }
 }
